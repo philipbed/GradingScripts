c91d0916dfe8a15022066b1f7c6add5dc4adab9c
Philip Bedward Jr <philipbed813@gmail.com>
2015-08-28 19:29:36 -0400

    Enters username

--- zip_download.py
+++ zip_download.py
@@ -1,5 +1,12 @@
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome('C:\chromedriver.exe')
 browser.get('https://mycourses.rit.edu')
+login_one = browser.find_element_by_name('username')
+login_two = browser.find_element_by_name('password')
+username = input("What is your username ?: ")
+login_one = login_one.send_keys(username)
+
+
