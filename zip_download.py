c187f42d79d64c93ba23535dc69ec4c5297b3b01
Philip Bedward Jr <philipbed813@gmail.com>
2015-08-29 14:11:24 -0400

    Facing challenges trying to switch to the pop-up window but properly selects all students and clicks the 'download' button.

--- zip_download.py
+++ zip_download.py
@@ -1,9 +1,18 @@
+"""
+Script that will sign into myCourses and download the
+zip files of all selected students
+
+Author: Philip Bedward
+"""
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+#open Chrome and load myCourses
 browser = webdriver.Chrome('C:\chromedriver.exe')
 browser.get('https://mycourses.rit.edu')
+
+#login to myCourses
 login_part_one = browser.find_element_by_name('username')
 login_part_two = browser.find_element_by_name('password')
 username = input("What is your username ?: ")
@@ -13,10 +22,18 @@ login_part_two = login_part_two.send_keys(password)
 submit = browser.find_element_by_name("f_submit")
 submit.click()
 
+#navigate to the proper Lab and filter the students
 input("Please navigate to proper Lab page")
 
+#Check all students 
 check_box = browser.find_element_by_name("z_h_cb_sa")
 check_box.click()
 download_button = browser.find_element_by_class_name("di_t")
 download_button.click()
+
+#handle pop-up window
+input("press enter after pop up window appears")
+browser.switch_to_window(browser.window_handles[1]) # problems switching to pop-up window
+zip_link = browser.find_element_by_partial_link_text("Lab")
+#zip_link.click()
                                                                
