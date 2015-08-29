40188b0ce744cab0d133f99d8c9e77fed765f86c
Philip Bedward Jr <philipbed813@gmail.com>
2015-08-29 12:02:58 -0400

    Can now retrieve zip file link

--- zip_download.py
+++ zip_download.py
@@ -12,3 +12,11 @@ login_part_one = login_part_one.send_keys(username)
 login_part_two = login_part_two.send_keys(password)
 submit = browser.find_element_by_name("f_submit")
 submit.click()
+
+input("Please navigate to proper Lab page")
+
+check_box = browser.find_element_by_name("z_h_cb_sa")
+check_box.click()
+download_button = browser.find_element_by_class_name("di_t")
+download_button.click()
+                                                               
