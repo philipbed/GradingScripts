5dc03075797909b1712670da5d72de8682a7706b
Philip Bedward Jr <philipbed813@gmail.com>
2015-08-28 20:04:41 -0400

    Submit button works

--- zip_download.py
+++ zip_download.py
@@ -4,9 +4,11 @@ from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome('C:\chromedriver.exe')
 browser.get('https://mycourses.rit.edu')
-login_one = browser.find_element_by_name('username')
-login_two = browser.find_element_by_name('password')
+login_part_one = browser.find_element_by_name('username')
+login_part_two = browser.find_element_by_name('password')
 username = input("What is your username ?: ")
-login_one = login_one.send_keys(username)
-
-
+password = input("What is your password ?: ")
+login_part_one = login_part_one.send_keys(username)
+login_part_two = login_part_two.send_keys(password)
+submit = browser.find_element_by_name("f_submit")
+submit.click()
