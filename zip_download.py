d352d6df42622378bacc7dc8a0d4c90117d7c0cc
Philip Bedward Jr <philipbed813@gmail.com>
2015-09-02 07:50:02 -0400

    added .gitignore and separated logic into functions

--- zip_download.py
+++ zip_download.py
@@ -7,46 +7,91 @@ Author: Philip Bedward
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 #open Chrome and load myCourses
 ##browser = webdriver.Chrome('C:\chromedriver.exe')
 ##browser.get('https://mycourses.rit.edu')
 
 # open Firefox and load myCourses
-browser = webdriver.Firefox()
-browser.get('https://mycourses.rit.edu')
+def login():
+    """
+    Opens the Firefox web browser loads myCourses and prompts the user to enter
+    their login credentials and clicks the submit button
+    :return:
+    """
 
-#login to myCourses
-login_part_one = browser.find_element_by_name('username')
-login_part_two = browser.find_element_by_name('password')
-username = input("What is your username ?: ")
-password = input("What is your password ?: ")
-login_part_one = login_part_one.send_keys(username)
-login_part_two = login_part_two.send_keys(password)
-submit = browser.find_element_by_name("f_submit")
-submit.click()
+    browser = webdriver.Firefox()
+
+
+    browser.get('https://mycourses.rit.edu')
+
+    #login to myCourses
+    login_part_one = browser.find_element_by_name('username')
+    login_part_two = browser.find_element_by_name('password')
+    username = input("What is your username ?: ")
+    password = input("What is your password ?: ")
+    login_part_one = login_part_one.send_keys(username)
+    login_part_two = login_part_two.send_keys(password)
+    submit = browser.find_element_by_name("f_submit")
+    submit.click()
 
 #navigate to the proper Lab and filter the students
-input("Please navigate to proper Lab page")
+def select_all():
+    """
+    Prompts the user to navigate to the proper lab page
+    then selects all students and clicks the download
+    button.
+    :return:
+    """
+    input("Please navigate to proper Lab page")
 
-#Check all students
-print(list(browser.window_handles))
-check_box = browser.find_element_by_name("z_h_cb_sa")
-check_box.click()
-download_button = browser.find_element_by_class_name("di_t")
-download_button.click()
+    #Check all students
+    print(list(browser.window_handles))
+    check_box = browser.find_element_by_name("z_h_cb_sa")
+    check_box.click()
+    download_button = browser.find_element_by_class_name("di_t")
+    download_button.click()
 
 
 #handle pop-up window
-input("press enter after pop up window appears")
-handles = list(browser.window_handles) 
-print(handles)
-handles.remove(browser.current_window_handle)
-print(handles)
-browser.switch_to_window(handles[0])
-print(browser.current_window_handle)
+def change_windows():
+    """
+    Gets the popup window's handle pattern and sets
+    that as the current window handle
+    """
+    # Store sll of the window handles into a list
+    handles = list(browser.window_handles)
 
-#switches to proper window but cannot find link element
-zip_link = browser.find_element_by_class_name("dfl")
-print(zip_link)
+    # Make sure there are two window handles
+    print("There are two window handles: ",len(handles)==2)
+
+    # Remove the current window handle from the list
+    handles.remove(browser.current_window_handle)
+
+    # Switch to the window handle of the popup window
+    browser.switch_to.window(handles[0])
+
+
+    print(browser.current_url) # double check the url
+
+# Switches to proper window but cannot find link element
+
+def download_all():
+    """
+    Clicks the download link for all zip files
+    :return:
+    """
+    try:
+        wait = WebDriverWait(browser,100)
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#z_b > a")))
+        zip_link = browser.find_element_by_css_selector("#z_b > a")
+
+    except Exception as e:
+        print(e)
+
+
+
                                                               
