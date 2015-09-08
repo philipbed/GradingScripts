c3f51d7dae7aabc9061ae45561410b94a505e5b9
Philip Bedward Jr <philipbed813@gmail.com>
2015-09-08 09:39:09 -0400

    Completed the script with full functionality

--- zip_download.py
+++ zip_download.py
@@ -1,97 +1,131 @@
+__author__ = 'Phil'
 """
-Script that will sign into myCourses and download the
-zip files of all selected students
+Script that will sign into myCourses, navigate
+to the proper Lab page, open the popup window
+that contains the zip file of all the students labs
+and downloads them.
 
 Author: Philip Bedward
+Date: 9/8/2015
 """
-import selenium
+
+
+
+
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-#open Chrome and load myCourses
-##browser = webdriver.Chrome('C:\chromedriver.exe')
-##browser.get('https://mycourses.rit.edu')
-
-# open Firefox and load myCourses
-def login():
-    """
-    Opens the Firefox web browser loads myCourses and prompts the user to enter
-    their login credentials and clicks the submit button
-    :return:
-    """
-
-    browser = webdriver.Firefox()
 
 
-    browser.get('https://mycourses.rit.edu')
+class Download:
 
-    #login to myCourses
-    login_part_one = browser.find_element_by_name('username')
-    login_part_two = browser.find_element_by_name('password')
-    username = input("What is your username ?: ")
-    password = input("What is your password ?: ")
-    login_part_one = login_part_one.send_keys(username)
-    login_part_two = login_part_two.send_keys(password)
-    submit = browser.find_element_by_name("f_submit")
-    submit.click()
+    def __init__( self, driver=webdriver.Firefox()):
+        self.browser = driver
+        self.url = driver.current_url
+        self.browser.implicitly_wait(10)
 
-#navigate to the proper Lab and filter the students
-def select_all():
-    """
-    Prompts the user to navigate to the proper lab page
-    then selects all students and clicks the download
-    button.
-    :return:
-    """
-    input("Please navigate to proper Lab page")
+    def open( self, link ):
+        self.browser.get(link)
 
-    #Check all students
-    print(list(browser.window_handles))
-    check_box = browser.find_element_by_name("z_h_cb_sa")
-    check_box.click()
-    download_button = browser.find_element_by_class_name("di_t")
-    download_button.click()
+    def login( self ):
+        """
+        Loads myCourses and prompts the user to enter
+        their login credentials and clicks the submit button
+        :return:
+        """
 
+        #login to myCourses
+        input("Please type in your login credential then press enter")
+        submit = self.browser.find_element_by_name("f_submit")
+        submit.click()
 
-#handle pop-up window
-def change_windows():
-    """
-    Gets the popup window's handle pattern and sets
-    that as the current window handle
-    """
-    # Store sll of the window handles into a list
-    handles = list(browser.window_handles)
+        def navigate_to_lab(self):
+            flyout = self.browser.find_element_by_css_selector("a.d2l-menuflyout-opener.d2l-clickable")
+            flyout.click()
 
-    # Make sure there are two window handles
-    print("There are two window handles: ",len(handles)==2)
+            computer_science_one = self.browser.find_element_by_partial_link_text("Computer Science I")
+            computer_science_one.click()
 
-    # Remove the current window handle from the list
-    handles.remove(browser.current_window_handle)
+            navbar = self.browser.find_elements_by_css_selector("a.d2l-navbar-link")
+            dropbox = navbar[8]
 
-    # Switch to the window handle of the popup window
-    browser.switch_to.window(handles[0])
+            dropbox.click()
+            lab = self.browser.find_element_by_link_text('Lab 1')
+            lab.click()
 
 
-    print(browser.current_url) # double check the url
-
-# Switches to proper window but cannot find link element
-
-def download_all():
-    """
-    Clicks the download link for all zip files
-    :return:
-    """
-    try:
-        wait = WebDriverWait(browser,100)
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#z_b > a")))
-        zip_link = browser.find_element_by_css_selector("#z_b > a")
-
-    except Exception as e:
-        print(e)
+    def select_all(self):
+        """
+        Prompts the user to navigate to the proper lab page
+        then selects all students and clicks the download
+        button.
+        :return:
+        """
 
 
+        #Check all students
+        check_box = self.browser.find_element_by_name("z_h_cb_sa")
 
-                                                              
+        check_box.click()
+
+        download_button = self.browser.find_element_by_class_name("di_t")
+
+        download_button.click()
+
+
+    #handle pop-up window
+    def change_windows(self):
+        """
+        Gets the popup window's handle pattern and sets
+        that as the current window handle
+        """
+
+        # Store all of the window handles into a list
+        handles = self.browser.window_handles
+
+        # Make sure there are two window handles
+
+        # Remove the current window handle from the list
+        handles.remove( self.browser.current_window_handle )
+
+        # Switch to the window handle of the popup window
+        self.browser.switch_to.window( self.browser.window_handles[1] )
+
+
+    def download_all(self):
+        frames = self.browser.find_elements_by_tag_name('frame')
+
+        frame = frames[1]
+        self.browser.switch_to.frame(frame)
+        download = self.browser.find_element_by_css_selector('span.dfl>a')
+        download.click()
+
+
+    def navigate_to_lab(self):
+        flyout = self.browser.find_element_by_css_selector("a.d2l-menuflyout-opener.d2l-clickable")
+        flyout.click()
+
+        computer_science_one = self.browser.find_element_by_partial_link_text("Computer Science I")
+        computer_science_one.click()
+
+        navbar = self.browser.find_elements_by_css_selector("a.d2l-navbar-link")
+        dropbox = navbar[8]
+
+        dropbox.click()
+        lab_number = input("what lab number is this?: ")
+        lab = self.browser.find_element_by_link_text('Lab '+lab_number)
+        lab.click()
+
+
+
+
+if __name__ == '__main__':
+
+    d = Download()
+    go_to = 'https://mycourses.rit.edu'
+    d.open(go_to)
+    d.login()
+    d.navigate_to_lab()
+    d.select_all()
+    d.change_windows()
+    d.download_all()
+
